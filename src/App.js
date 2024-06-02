524454097b1a197550425ffde93e8a49c4b95eb4
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-06-03 01:15:46 +0300

    styled and created form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <main className="h-screen flex items-center justify-center ">
+        <form className="bg-white flex rounded-lg w-1/2 font-latoRegular"></form>
+        <div className="flex-1 text-gray-700 p-20">
+          <h1 className="text-red-200">Let's get started 👋</h1>
+          <p>Please put down your information for futher appointment</p>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
